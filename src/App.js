8046ec6ff9ad53be8c101ce4505f2c75fc0c4840
Camila Cardona <kamialeja24@gmail.com>
2023-04-12 21:54:06 -0500

    Add state object and do shallow merge

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: {firstName: 'Camila', lastName: 'Cardona' },
+      company: 'ZTM'
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, the company is {this.state.company}</p>
+          <button onClick={() => {
+            this.setState({ name: {firstName: 'Diego', lastName: 'Ribero'} })
+            console.log('STATE >>> ', this.state);
+          }}>Change Name</button>
         </header>
       </div>
     );
