e3ca5353d2e8459c1b49eda24e4dcff613af5009
Camila Cardona <kamialeja24@gmail.com>
2023-04-13 22:54:40 -0500

    Filter out users list

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      filteredMonsters: [],
     };
   }
 
@@ -14,7 +15,7 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(() => {
-        return { monsters: users }
+        return { monsters: users, filteredMonsters: users }
       },
         () => {
           console.log('Callback >>> ', this.state);
@@ -26,8 +27,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input className='search-box' type="search" placeholder='Search monsters' onChange={(event) => {
+          const filMon = this.state.monsters.filter((monster) => {
+            return (
+              monster.name.toLowerCase().includes(event.target.value.toLowerCase())
+              );
+            })
+          this.setState(() => {
+            return {
+              filteredMonsters: filMon
+            }
+          }, () => {
+            console.log('VE', this.state.filteredMonsters);
+          })
+        }} />
         {
-          this.state.monsters.map((monster) => {
+          this.state.filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
