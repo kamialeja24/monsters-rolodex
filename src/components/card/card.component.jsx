582980976fd60cc4586043ed170b76e0305bdd1b
Camila Cardona <kamialeja24@gmail.com>
2023-05-02 22:22:24 -0500

    Improve overall app

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,7 @@ import './card.styles.css'
 
 class Card extends Component {
   render() {
-    const { monster } = this.props;
-    const { name, email, id } = monster;
+    const { name, email, id } = this.props.monster;
     return (
     <div className='card-container' key={id}>
       <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`Monster ${name}`} />
