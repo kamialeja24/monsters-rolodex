7740bee37e0a2205899d823a0a1168a488162980
Camila Cardona <kamialeja24@gmail.com>
2023-05-16 22:32:23 -0500

    Turn all class components into functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,20 +2,14 @@ import { Component } from 'react';
 import Card from '../card/card.component'
 import './card-list.styles.css'
 
-class CardList extends Component {
-  render() {
-    console.log('render from CardList');
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return (
-            <Card monster={monster} key={monster.id}  />
-          )
-        })}
-      </div>
-    )
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return (
+        <Card monster={monster} key={monster.id}  />
+      )
+    })}
+  </div>
+)
 
 export default CardList;
