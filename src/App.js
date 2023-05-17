ef3e68c537c3c8e4627fb99c79821c397a37a207
Camila Cardona <kamialeja24@gmail.com>
2023-05-16 22:18:46 -0500

    Add more useEffects and make the app properly work with them

--- src/App.js
+++ src/App.js
@@ -9,21 +9,26 @@ const App = () => {
   console.log('Render');
   const [searchField, setSearchField] = useState(''); // [value, setValue]
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  console.log('Render');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
 
-  // fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then(response => response.json())
-  //   .then((users) => setMonsters(users));
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+
+  }, [monsters, searchField])
 
   const onSearchChange = (event) => {
      const searchFieldString = event.target.value.toLocaleLowerCase();
      setSearchField(searchFieldString);
   }
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    })
 
   return (
     <div className="App">
